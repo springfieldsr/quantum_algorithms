e9b5b0bc06bb486d5cd4dc26ef0cf952f31c009a
vinares <877649518@qq.com>
2022-02-14 17:46:56 -0800

    Annotate the Bernstein_Vazirani algorithm

--- Bernstein_Vazirani.py
+++ Bernstein_Vazirani.py
@@ -9,12 +9,12 @@ def bernstein_vazirani_circuit(f, n):
 
 
 def bernstein_vazirani_solver(f, n):
-    circuit = bernstein_vazirani_circuit(f, n)
-    simulator = Simulator()
-    result = simulator.run(circuit)
+    circuit = bernstein_vazirani_circuit(f, n)          # The structure of the quantum circuit is the same as in Deutsch-Josza
+    simulator = Simulator()                             # Instantiate a simulator variable
+    result = simulator.run(circuit)                     # Run the circuit on the simulator and save the result
 
-    measurements = result.data.values.tolist()[0]
-    a = "".join([str(i) for i in measurements])
-    b = str(f("0"))
+    measurements = result.data.values.tolist()[0]       # Measure first n qubits and save them to a list, which is the "a" we are looking for
+    a = "".join([str(i) for i in measurements])         # Save a as a string
+    b = str(f("0"))                                     # Obtain b by calling f with input "0"
 
     return a, b
