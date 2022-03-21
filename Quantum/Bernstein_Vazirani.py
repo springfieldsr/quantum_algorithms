98c0a23b41043b09716e7a15fbd580bbe3e70cb0
LostnEkko <nekobytz@gmail.com>
2022-03-20 22:14:29 -0700

    BV and DJ modified qasm

--- Quantum/Bernstein_Vazirani.py
+++ Quantum/Bernstein_Vazirani.py
@@ -10,11 +10,14 @@ def bernstein_vazirani_circuit(f, n):
 
 def bernstein_vazirani_solver(f, n):
     circuit = bernstein_vazirani_circuit(f, n)          # The structure of the quantum circuit is the same as in Deutsch-Josza
+    print(circuit.to_qasm())
     simulator = Simulator()                             # Instantiate a simulator variable
     result = simulator.run(circuit)                     # Run the circuit on the simulator and save the result
 
     measurements = result.data.values.tolist()[0]       # Measure first n qubits and save them to a list, which is the "a" we are looking for
     a = "".join([str(i) for i in measurements])         # Save a as a string
     b = str(f("0"))                                     # Obtain b by calling f with input "0"
+    print(measurements)
+
 
     return a, b
